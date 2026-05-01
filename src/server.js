3de29ddb3e8286922ec129ba44fa7477e9ba7693
unknown <aquanataliya@gmail.com>
2026-05-01 19:09:26 +0300

    add console.log for PORT

--- src/server.js
+++ src/server.js
@@ -47,4 +47,6 @@ app.use((err, req, res) => {
   res.status(500).json({ message: err.message });
 });
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
