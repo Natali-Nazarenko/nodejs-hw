4d734fd1fb52433cd68292a91b555778ebdc6405
unknown <aquanataliya@gmail.com>
2026-05-12 23:11:07 +0300

    add DELETE and PATCH

--- src/server.js
+++ src/server.js
@@ -1,55 +1,27 @@
 import express from 'express';
 import cors from 'cors';
-import pino from 'pino-http';
 import 'dotenv/config';
 
 import { connectMongoDB } from './db/connectMongoDB.js';
-import { Note } from './models/note.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import { logger } from './middleware/logger.js';
+import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
+app.use(logger);
 app.use(express.json());
-app.use(cors());
 app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat:
-          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
+  cors({
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   }),
 );
 
-app.get('/notes', async (req, res) => {
-  const notes = await Note.find();
-  res.status(200).json(notes);
-});
-
-app.get('/notes/:noteId', (req, res) => {
-  res
-    .status(200)
-    .json({ message: `Retrieved note with ID: ${req.params.noteId}` });
-});
-
-app.get('/test-error', () => {
-  throw new Error('Simulated server error');
-});
-
-app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
-});
-
-app.use((err, req, res) => {
-  res.status(500).json({ message: err.message });
-});
+app.use(notesRoutes);
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 await connectMongoDB();
 
