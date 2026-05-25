f9c733a43487565bbada69542163ae04b418b3d8
unknown <aquanataliya@gmail.com>
2026-05-25 23:46:19 +0300

    update search notes

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -14,10 +14,10 @@ export const getAllNotes = async (req, res) => {
   }
 
   if (search) {
-    notesQuery.$or = [
+    notesQuery.or([
       { title: { $regex: search, $options: 'i' } },
       { content: { $regex: search, $options: 'i' } },
-    ];
+    ]);
   }
 
   const [totalNotes, notes] = await Promise.all([
